8d8ff2168bdc793fc471ef6ec98f94c259f9db5d
j0verton <overtonje@gmail.com>
2020-08-31 14:16:44 -0400

    mvp chpt 10

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -5,15 +5,49 @@ export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
             <button id="rideTicket">Ride Ticket</button>
+            <button id="foodTicket">Food Ticket</button>
+            <button id="gameTicket">Game Ticket</button>
+            <button id="sideshowTicket">Sideshow Ticket</button>
         </div>
     `
     contentTarget.addEventListener("click", (e) => {
         const idOfClickedButton = e.target.id
-        if (idOfClickedButton === "rideTicket"){
-            const rideEvent = new CustomEvent("rideTicketPurchased");
-            eventHub.dispatchEvent(rideEvent);
-
+        switch (idOfClickedButton) {
+            case "rideTicket":
+                const rideEvent = new CustomEvent("rideTicketPurchased");
+                eventHub.dispatchEvent(rideEvent);
+                break;
+            case "foodTicket":
+                const foodEvent = new CustomEvent("foodTicketPurchased");
+                eventHub.dispatchEvent(foodEvent);
+                break;
+            case "gameTicket":
+                const gameEvent = new CustomEvent("gameTicketPurchased");
+                eventHub.dispatchEvent(gameEvent);
+                break;
+            case "sideshowTicket":
+                const sideshowEvent = new CustomEvent("sideshowTicketPurchased");
+                eventHub.dispatchEvent(sideshowEvent);
+                break;
+                                            
+            default:
+                break;
         }
-    
+        
+        
+        
+        
+        
+        // if (idOfClickedButton === "rideTicket"){
+        //     const rideEvent = new CustomEvent("rideTicketPurchased");
+        //     eventHub.dispatchEvent(rideEvent);
+
+        // } else if (idOfClickedButton === "foodTicket"){
+        //     const foodEvent = new CustomEvent("foodTicketPurchased");
+        //     eventHub.dispatchEvent(foodEvent);
+
+        // } else if 
     });
+
 }
+
