1278d3a8ddf7467a0c865c81cf3d21de321905e5
j0verton <overtonje@gmail.com>
2020-08-31 13:27:57 -0400

    first cmmit

---
+++ scripts/TicketBooth.js
@@ -0,0 +1,23 @@
+const contentTarget = document.querySelector(".entry")
+const eventHub = document.querySelector("#state-fair")
+
+
+
+
+
+export const TicketBooth = () => {
+    contentTarget.innerHTML = `
+        <div class="ticketBooth">
+            <button id="rideTicket">Ride Ticket</button>
+        </div>
+    `
+    contentTarget.addEventListener("click", (e) => {
+        const idOfClickedButton = e.target.id
+        if (idOfClickedButton === "rideTicket"){
+            const rideEvent = new CustomEvent("rideTicketPurchased");
+            eventHub.dispatchEvent(rideEvent);
+
+        }
+    
+    });
+}
